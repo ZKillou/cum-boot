ba95becc36284ca8ee0a99be4133baa47af7e665
ZKillou <2econtedekillian@gmail.com>
2022-05-21 23:52:43 +0200

    even more feur imagesgit commit -m

--- src/utils/quickreplies.js
+++ src/utils/quickreplies.js
@@ -9,7 +9,13 @@ module.exports = function (message) {
 
   if(content.endsWith("quoi") || content.endsWith("quoi ?") || content.endsWith("quoi?") ||
   content.endsWith("what") || content.endsWith("what ?") || content.endsWith("what?")) {
-    return message.reply("https://cdn.discordapp.com/attachments/827593601541865562/976860929855717386/caption.jpg")
+    let links = [
+      "https://cdn.discordapp.com/attachments/827593601541865562/976860929855717386/caption.jpg",
+      "https://cdn.discordapp.com/attachments/790552353119535104/977687447297200168/caption.png",
+      "https://cdn.discordapp.com/attachments/790552353119535104/977689518742306837/caption.webp",
+      "https://cdn.discordapp.com/attachments/790552353119535104/977689987724218368/caption.png"
+    ]
+    return message.reply(links[Math.floor(Math.random() * links.length)])
   } else if (content.includes("amogus")) {
     return message.reply("sus bro")
   } else if(content.endsWith("sussy")) {
